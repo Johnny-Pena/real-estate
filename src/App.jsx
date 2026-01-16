7b6d6dc116f0efd1fda1b5c320b0ab6addab977e
Johnny Pena <jpenaguitarist@gmaio.com>
2026-01-15 20:48:59 -0600

    removed Google ads information

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Navbar from './components/layout/Navbar.jsx';
 import Footer from './components/layout/Footer.jsx';
 import Bio from './routes/bio.jsx';
 import Contact from './routes/contact.jsx';
-import AnalyticsTracker from './components/AnalyticsTracker.jsx';
-import TestimonialsPage from './routes/testimonialsPage.jsx';
 import PrivacyPolicy from './routes/privacy-policy.jsx';
 import TermsOfService from './routes/terms-of-service.jsx';
 import Apartments from './routes/apartments.jsx';
@@ -28,7 +26,6 @@ function App() {
 <Router>
   <ScrollToTop />
   <div>
-  <AnalyticsTracker />
     {/* <MetaPixel /> */}
     <Navbar />
     <Routes>
